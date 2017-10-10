f678fc1524ae176cbb9a00d55881c174d2e7af0c
Krishnan Pontes <krishnanpontes@gmail.com>
2017-10-10 19:16:28 +0000

    Try to make jquery work on heroku 2

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -1,10 +1,11 @@
 /*global $*/
+    //$(document).on('turbolinks:load', function() { 
     //$(document).ready(function() {
     //$("#number_number").css("background-color", "yellow");
     //$(".form-group").css("color", "blue");
     //$("#number_save").bind('click', function( event ){ 
 
-$(document).on('turbolinks:load', function() {    
+$(document).ready(function() {  
     var montaNum = function(){
         var number = $("#number_countrycode").val() +
                      $("#number_cn").val() +
